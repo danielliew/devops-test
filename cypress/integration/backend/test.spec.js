a6c3552a02dd261c2fd1ba258bcb4091f430e1c9
danielliew <danieliewjh@gmail.com>
2021-05-25 13:32:23 +0800

    cypress run fix

--- cypress/integration/backend/test.spec.js
+++ cypress/integration/backend/test.spec.js
@@ -2,24 +2,20 @@
 
 describe("Backend API GET /", () => {
   it("returns status 200", () => {
-    cy.request("http://localhost:3001").its("status").should("equal", 200);
+    cy.request("/").its("status").should("equal", 200);
   });
 
   it("returns hello world", () => {
-    cy.request("http://localhost:3001")
-      .its("body")
-      .should("equal", "Hello world");
+    cy.request("/").its("body").should("equal", "Hello world");
   });
 });
 
 describe("Backend API GET /json", () => {
   it("returns status 200", () => {
-    cy.request("http://localhost:3001").its("status").should("equal", 200);
+    cy.request("/").its("status").should("equal", 200);
   });
 
   it("returns JSON hello world", () => {
-    cy.request("http://localhost:3001/json")
-      .its("body.msg")
-      .should("equal", "Hello world");
+    cy.request("/json").its("body.msg").should("equal", "Hello world");
   });
 });
